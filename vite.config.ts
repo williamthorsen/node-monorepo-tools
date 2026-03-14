c40f9bfef5ad9159acfe4de8d2c7b61ffd5bcdab
William Thorsen <william@thorsen.dev>
2026-03-14 10:26:28 -0700

    root|deps: Upgrade Vite to v8

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
-import createTsconfigPathsPlugin from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    // Automatically create aliases to match those definedi in `paths` in `tsconfig.json`.
-    createTsconfigPathsPlugin(),
-  ],
   server: {
     watch: {
       ignored: ['**/coverage/**'],
