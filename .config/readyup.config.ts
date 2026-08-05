d7c246691646f703a86ef4380ecf3768b294d725
William Thorsen <william@thorsen.dev>
2026-08-04 21:42:18 -0700

    root|deps: Upgrade all deps to latest version

--- .config/readyup.config.ts
+++ .config/readyup.config.ts
@@ -2,5 +2,6 @@ import { defineRdyConfig } from 'readyup';
 
 /** Readyup configuration for this monorepo. */
 export default defineRdyConfig({
+  // The checks in these packages will be run by `rdy run --packages`.
   packages: ['@williamthorsen/nmr', '@williamthorsen/release-kit', 'v11y-check'],
 });
